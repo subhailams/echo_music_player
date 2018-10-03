0f7c88350b84253f52b78e39f77604819229d201
subha <subh16322.cs@rmkec.ac.in>
2018-10-03 19:51:57 +0530

    built navigation drawer

--- app/src/main/java/com/ilamathy/echo/activities/MainActivity.kt
+++ app/src/main/java/com/ilamathy/echo/activities/MainActivity.kt
@@ -4,15 +4,27 @@ import android.os.Bundle
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import com.ilamathy.echo.R
+import com.ilamathy.echo.adapters.NavigationDrawerAdapter
 import com.ilamathy.echo.fragments.MainScreenFragment
 
 class MainActivity : AppCompatActivity(){
     /*This Line is use to define the variable for drawer layout*/
-    var drawerLayout: DrawerLayout?=null
+    var navigationDrawerIconsList : ArrayList<String> = arrayListOf()
+
+    var images_for_navdrawer = intArrayOf(R.drawable.navigation_allsongs, R.drawable.navigation_favorites,
+            R.drawable.navigation_settings, R.drawable.navigation_aboutus)
+    object Statified{
+        var drawerLayout: DrawerLayout?=null
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -22,7 +34,14 @@ class MainActivity : AppCompatActivity(){
         /*Tells compiler that the actionbar will be interactive*/
         setSupportActionBar(toolbar)
 
-        drawerLayout = findViewById(R.id.drawer_layout)
+        MainActivity.Statified.drawerLayout = findViewById(R.id.drawer_layout)
+
+
+        navigationDrawerIconsList.add("All Songs")
+        navigationDrawerIconsList.add("Favorites")
+        navigationDrawerIconsList.add("Settings")
+        navigationDrawerIconsList.add("About Us")
+
 
         /*The icon having 3 parallel horizontal lines is known as the action bar drawer
         toggle. The 4 parameters taken by the this can be understood as the:
@@ -33,7 +52,7 @@ class MainActivity : AppCompatActivity(){
         and close the drawer*/
 
 
-        val toggle = ActionBarDrawerToggle(this@MainActivity,drawerLayout,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        val toggle = ActionBarDrawerToggle(this@MainActivity,MainActivity.Statified.drawerLayout,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
 
         /*Earlier we only defined the toggle, now we place the listener using the
         addDrawerListener() method
@@ -42,7 +61,7 @@ class MainActivity : AppCompatActivity(){
          * can be removed from the future versions, hence here we have told the latest
         version of the method i.e. addDrawerListener(). Although as of now the usage
         * setDrawerListener() is also correct*/
-        drawerLayout?.setDrawerListener(toggle)
+        MainActivity.Statified.drawerLayout?.setDrawerListener(toggle)
         toggle.syncState()
 
         val mainScreenFragment = MainScreenFragment()
@@ -50,6 +69,18 @@ class MainActivity : AppCompatActivity(){
                 .beginTransaction()
                 .add(R.id.details_fragment, mainScreenFragment, "MainScreenFragment")
                 .commit()
+
+        var _navigationAdapter = NavigationDrawerAdapter(navigationDrawerIconsList, images_for_navdrawer,this)
+        _navigationAdapter.notifyDataSetChanged()
+
+          var navigation_recycler_view =findViewById<RecyclerView>(R.id.navigation_recycler_view)
+          navigation_recycler_view.layoutManager= LinearLayoutManager(this)
+          navigation_recycler_view.itemAnimator = DefaultItemAnimator()
+
+        navigation_recycler_view.adapter = _navigationAdapter
+        navigation_recycler_view.setHasFixedSize(true)
+
+
     }
 
     override fun onStart() {
